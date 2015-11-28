8f81efc7856c26ecd5994390ed1657f4cf33f6e7
Janos Potecki <janos.potecki.15@ucl.ac.uk>
2015-11-28 16:11:59 +0000

    Everything done but bruteforce

--- haskell/LabSheet2/LabSheet2.hs
+++ haskell/LabSheet2/LabSheet2.hs
@@ -12,7 +12,7 @@ countPositives (x:xs)     | x > 0       = 1 + countPositives xs
                           | otherwise   = 0 + countPositives xs   
 
 length' :: [a] -> Int
-length' xs = sum[1 | _ <- xs]
+length' xs = sum[ 1 | _ <- xs]
 
 capitalised :: String -> String
 capitalised (x:xs) = [toUpper x] ++ toLower' xs
@@ -47,3 +47,34 @@ msort xs =  merge
                 (msort(take (length' xs `div` 2) xs))
                 (msort(drop (length' xs `div` 2) xs))
             
+rotor :: Int -> [a] -> [a]
+rotor 0 xs = xs
+rotor r xs  | r < 0             = error "smaller than 0"
+            | r >= (length' xs) = error ">= than length' xs"
+            | otherwise         = rotor (r - 1) (tail xs ++ [head xs]) 
+
+makeKey :: Int -> [(Char, Char)]
+makeKey x = zip ['A'..'Z'] (rotor x ['A'..'Z'])
+
+lookUp :: Char -> [(Char, Char)] -> Char
+lookUp c xs | c `elem` ['A'..'Z']   = snd (head [x | x <- xs, fst x == c])
+            | otherwise             = c
+
+encipher :: Int -> Char -> Char
+encipher x c = lookUp c (makeKey x)
+
+normalise :: String -> String
+normalise [] = []
+normalise (x:xs)    | x `elem` ['A'..'Z']   = x : normalise xs
+                    | x `elem` ['0'..'9']   = x : normalise xs
+                    | x `elem` ['a'..'z']   = toUpper x : normalise xs
+                    | otherwise             = normalise xs
+
+encipherStr :: Int -> String -> String
+encipherStr a str = [encipher a x | x <- (normalise str)]
+
+
+
+
+
+
