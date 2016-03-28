09036bdabdf8407753b6c105dfd2838ebb242f69
Janós Potecki <janospotecki@yahoo.de>
2016-03-28 23:57:49 +0200

    nim 2 players added

--- haskell/IO/playground.hs
+++ haskell/IO/playground.hs
@@ -1,38 +1,16 @@
-import Data.Char
-import Control.Monad
+ins :: Integer -> [Integer] -> [Integer]
+ins x [] = [x]
+ins x (y:ys) | x <= y    = x : (y:ys)
+             | otherwise = y : ins x ys
 
+iSort :: [Integer] -> [Integer]
+iSort [] = []
+iSort (x:xs) = ins x (iSort xs)
 
+xor :: Bool -> Bool -> Bool
+xor True True = False
+xor False False = False
+xor a b = True
 
-main = do
-    game $ [0..5]
-
-game :: [Int] -> IO ()
-game xs = when (gameNotFinished xs) $ do
-            putStrLn " "
-            printGameField xs
-            putStrLn "Row: "
-            row <- getLine
-            --putChar row
-            putStrLn "num of stones: "
-            stones <- getLine
-            let r = digitToInt (row!!0)
-                s = digitToInt (stones!!0)
-            --putChar stones
-            game $ updateGameField r s xs
-
-gameNotFinished :: [Int] -> Bool
-gameNotFinished xs = foldr1 (||) $ map (/=0) xs
-
--- takes a gamefield, row, and how may stones should be removed
-updateGameField::Int -> Int -> [Int] -> [Int]
-updateGameField r s xs = fst (splitAt (r) xs) ++  [(xs!!r) - s] ++  snd (splitAt (r+1) xs)
-
-printGameField :: [Int] -> IO ()
-printGameField xs = printGameField' [5,4..0] xs
--- helper function
-printGameField' :: [Int] ->[Int] -> IO ()
-printGameField' num (x:[]) = return ()
-printGameField' (n:num) xs = do  putStrLn $ unwords $ [show n] ++ replicate x "*"
-                                 printGameField' num (init xs)
-    where
-        x = last xs
+combi :: (Num a, Ord a) => [(a, a)] -> [a]
+combi xs = [sum[ fst x | x <- xs , (fst x) > (snd x)]] ++ [sum[ snd x | x <- xs, (fst x) > (snd x)]]
