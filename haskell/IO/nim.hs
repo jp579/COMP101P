d07348a5bb7ff13535d3e7c8d85b72bc82160e59
Janós Potecki <janospotecki@yahoo.de>
2016-04-02 17:42:56 +0200

    rename prity to kernel

--- haskell/IO/nim.hs
+++ haskell/IO/nim.hs
@@ -2,11 +2,10 @@ import Data.Char
 import Control.Monad
 
 --http://www.cdf.toronto.edu/~ajr/270/probsess/03/strategy.html
---TODO: just allow each player to take away 1,3, or 4 stones
---TODO: write brute force algorithm for AI
+--TODO: game crashes when player makes move 5,1 at the beginning
 
 main = do
-    game 0 [0..5]
+    game 1 [0..5]
 
 -- Player -> Gamefield -> IO ()
 game :: Int -> [Int] -> IO ()
@@ -14,27 +13,43 @@ game p xs = do
             let p'  | p == 0    = 1
                     | otherwise = 0
             if (gameNotFinished xs) then do
-                print $ "Player " ++ (show (p+1)) ++ "'s turn"
-                putStrLn " "
-                printGameField xs
-                putStrLn "Row: "
-                row <- getLine
-                --putChar row
-                putStrLn "num of stones: "
-                stones <- getLine
-                let r = digitToInt (row!!0)
-                    s = digitToInt (stones!!0)
-                -- check whether the move is a valid one
-                -- r is beteen rows 1 and 5 and if there are enough
-                -- stones left in the row
-                if (movePossible xs r s) then
-                    game p' $ updateGameField r s xs
+                if p == 0 then do
+                    printGameField xs
+                    print $ "player in Kernel:"
+                    print $  parity $ map (toBinary) xs
+                    print $ "Player " ++ (show (p+1)) ++ "'s turn"
+                    putStrLn " "
+
+                    putStrLn "Row: "
+                    row <- getLine
+                    --putChar row
+                    putStrLn "num of stones: "
+                    stones <- getLine
+                    let r = digitToInt (row!!0)
+                        s = digitToInt (stones!!0)
+                    -- check whether the move is a valid one
+                    -- r is beteen rows 1 and 5 and if there are enough
+                    -- stones left in the row
+                    if (movePossible xs r s) then
+                        game p' $ updateGameField r s xs
+                    else do
+                        putStrLn ""
+                        print "not valid input"
+                        game p xs
                 else do
-                    putStrLn ""
-                    print "not valid input"
-                    game p xs
+                    printGameField xs
+                    print "computer in Kernel:"
+                    print $ parity $ map (toBinary) xs
+                    let move    = botMove $ map (toBinary) xs
+                        r       = fst move
+                        s       = snd move
+                    print $ "Computer moved to" ++ (show move)
+                    game p' $updateGameField r s xs
 
             else print $ "finished : Player " ++ (show (p'+1)) ++ " lost"
+
+
+
 
 gameNotFinished :: [Int] -> Bool
 gameNotFinished xs = foldr1 (||) $ map (/=0) xs
@@ -85,15 +100,18 @@ parity' xss y = [(sum[(xs!!(y-1)) | xs <- xss]) `mod` 2] ++ parity' xss (y-1)
 -- check whether AI is in kernel, if yes -> make intelligent move
 -- else make random move
 -- return a tuple, with a (row, stones)
-checkKernel :: [[Int]] -> (Int, Int)
-checkKernel xss | parity xss    = randomMove xss
-                | otherwise     = findMove xss
+botMove :: [[Int]] -> (Int, Int)
+botMove xss | parity xss    = randomMove xss
+            | otherwise     = findMove xss
 
+-- this tries to bring the computer into a state from which the player
+-- cannot move into a parity
 findMove :: [[Int]] -> (Int, Int)
 findMove xss = findMove' xss 1 $ (+) (-1) $ length xss
     where
-        findMove' xss s r | s > (fromBinary (xss!!r))    = findMove' xss 1 (r-1)
-                          | parity (updateGameFieldBinary xss s r)      = (r, s)
+        findMove' xss s r | r < 0                        = randomMove xss
+                          | s > (fromBinary (xss!!r))    = findMove' xss 1 (r-1)
+                          | not (parity (updateGameFieldBinary xss s r))      = (r, s)
                           | otherwise                    = findMove' xss (s+1) r
 
 updateGameFieldBinary :: [[Int]] -> Int -> Int -> [[Int]]
@@ -106,7 +124,12 @@ newBinField xss s r =  map (toBinary) $ updateGameField r s $ map (fromBinary) x
 
 
 randomMove :: [[Int]] -> (Int, Int)
-randomMove xss = (2,1)
+randomMove xss = iterator xss 0
+    where
+        iterator :: [[Int]] -> Int -> (Int, Int)
+        iterator (xs:xss) r | sum xs == 0   = iterator xss (r+1)
+                            | otherwise     = (r, (sum xs))
+
 
 movePossible :: [Int] -> Int -> Int -> Bool
 movePossible xs r s | r < (length xs) = (<=) s $ xs!!r
